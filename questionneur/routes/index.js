cebe3752175e3b505f2c0e1e3ca615b7ce92341f
livilynx <anthony.2lannoy@gmail.com>
2014-11-01 22:06:26 -0400

    fichier jade fonctionnel mais à compléter. Les routes marchent.

---
+++ questionneur/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/accueil', function(req, res) {
+  res.render('accueil');
+});
+
+router.get('/question', function(req, res) {
+  res.render('question');
+});
+
+router.get('/examenTermine', function(req, res) {
+  res.render('examenTermine');
+});
+
+router.get('/tableauBord', function(req, res) {
+  res.render('tableauBord');
+});
+
+router.get('/instructions', function(req, res) {
+  res.render('instructions');
+});
+
+module.exports = router;
