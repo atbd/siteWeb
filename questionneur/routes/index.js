207a921b087bb6bed50d57bfcbe5a41124a778c6
super7ramp <antoine.belvire@laposte.net>
2014-11-07 15:32:39 -0500

    Redirection index->accueil

--- questionneur/routes/index.js
+++ questionneur/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+  res.redirect('accueil');
 });
 
 router.get('/accueil', function(req, res) {
